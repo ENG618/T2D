0b3e63acadf5ac12763feab0e425ba5fa332989c
eng618 <eng618@fullsail.edu>
2015-01-17 21:14:49 -0500

    Created receiver to obtain notification on batter status changes.  Created package for data, and started to create a Device class.

---
+++ T2D/app/src/main/java/com/garciaericn/t2d/data/Device.java
@@ -0,0 +1,17 @@
+package com.garciaericn.t2d.data;
+
+/**
+ * Full Sail University
+ * Mobile Development BS
+ * Created by ENG618-Mac on 1/17/15.
+ */
+public class Device {
+
+    public String deviceName;
+    public float currentBateryLevel;
+
+    public Device() {
+    }
+
+
+}
