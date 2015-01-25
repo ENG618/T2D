bb8ec8d15827c3a91b9b9b5cfc41880638d7df18
eng618 <eng618@fullsail.edu>
2015-01-24 23:43:29 -0500

    Started to integrate Parse

---
+++ T2D/app/src/main/java/com/garciaericn/t2d/T2DApplication.java
@@ -0,0 +1,22 @@
+package com.garciaericn.t2d;
+
+import android.app.Application;
+
+
+/**
+ * Full Sail University
+ * Mobile Development BS
+ * Created by ENG618-Mac on 1/24/15.
+ */
+public class T2DApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+//        Parse.enableLocalDatastore(this);
+//
+//        Parse.initialize(this, "mlbATdfRQ2RMJmNaVjjDcIybrtKnlEmic94bBL6r", "20c54czHXPKcb4ErBfbQe9q9vE9kaf6zS0A4GeLA");
+    }
+}
