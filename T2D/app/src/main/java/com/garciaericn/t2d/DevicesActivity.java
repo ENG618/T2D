b25553250756feb27f4f07359cfbd081d55f6315
eng618 <eng618@fullsail.edu>
2015-01-25 01:53:52 -0500

    Started to set up Parse for push notifications and user login

--- T2D/app/src/main/java/com/garciaericn/t2d/DevicesActivity.java
+++ T2D/app/src/main/java/com/garciaericn/t2d/DevicesActivity.java
@@ -31,24 +31,29 @@ public class DevicesActivity extends Activity implements
         adView.loadAd(adRequest);
 
 
-        ParseUser user = new ParseUser();
-        user.setUsername("my name");
-        user.setPassword("my pass");
-        user.setEmail("email@example.com");
-
-// other fields can be set just like with ParseObject
-        user.put("phone", "650-555-0000");
-
-        user.signUpInBackground(new SignUpCallback() {
-            public void done(ParseException e) {
-                if (e == null) {
-                    // Hooray! Let them use the app now.
-                } else {
-                    // Sign up didn't succeed. Look at the ParseException
-                    // to figure out what went wrong
-                }
-            }
-        });
+//        ParseUser user = new ParseUser();
+//        user.setUsername("my name");
+//        user.setPassword("my pass");
+//        user.setEmail("email@example.com");
+//
+//        // other fields can be set just like with ParseObject
+//        user.put("phone", "650-555-0000");
+//
+//        user.signUpInBackground(new SignUpCallback() {
+//            public void done(ParseException e) {
+//                if (e == null) {
+//                    // Hooray! Let them use the app now.
+//                } else {
+//                    // Sign up didn't succeed. Look at the ParseException
+//                    // to figure out what went wrong
+//                }
+//            }
+//
+//            @Override
+//            public void done(com.parse.ParseException e) {
+//
+//            }
+//        });
 
         getFragmentManager().beginTransaction()
                 .replace(R.id.list_container, DevicesCardViewFragment.newInstance())
