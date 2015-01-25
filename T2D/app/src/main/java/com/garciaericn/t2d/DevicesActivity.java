ca27ddafd2e97ac7a2c535f45c52d0b2b1033500
eng618 <eng618@fullsail.edu>
2015-01-25 00:21:00 -0500

    Removed dummy data and list fragment

--- T2D/app/src/main/java/com/garciaericn/t2d/DevicesActivity.java
+++ T2D/app/src/main/java/com/garciaericn/t2d/DevicesActivity.java
@@ -7,10 +7,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.garciaericn.t2d.fragments.DevicesCardViewFragment;
-import com.garciaericn.t2d.fragments.DevicesListFragment;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import java.text.ParseException;
 
 
 public class DevicesActivity extends Activity implements
@@ -29,9 +31,27 @@ public class DevicesActivity extends Activity implements
         adView.loadAd(adRequest);
 
 
+        ParseUser user = new ParseUser();
+        user.setUsername("my name");
+        user.setPassword("my pass");
+        user.setEmail("email@example.com");
+
+// other fields can be set just like with ParseObject
+        user.put("phone", "650-555-0000");
+
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                }
+            }
+        });
+
         getFragmentManager().beginTransaction()
                 .replace(R.id.list_container, DevicesCardViewFragment.newInstance())
-//                .replace(R.id.list_container, DevicesListFragment.newInstance("test", "testing"))
                 .commit();
     }
 
