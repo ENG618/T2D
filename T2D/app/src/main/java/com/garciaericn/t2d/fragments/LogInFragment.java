3a41326d7d604540fa39f415c6e2e083143ef5f7
eng618 <eng618@fullsail.edu>
2015-01-27 00:43:38 -0500

    Created new fragments, for signing up and logging in.

---
+++ T2D/app/src/main/java/com/garciaericn/t2d/fragments/LogInFragment.java
@@ -0,0 +1,17 @@
+package com.garciaericn.t2d.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Full Sail University
+ * Mobile Development BS
+ * Created by ENG618-Mac on 1/27/15.
+ */
+public class LogInFragment extends Fragment {
+
+    public LogInFragment() {
+        //Mandatory empty constructor
+    }
+
+
+}
---
+++ T2D/app/src/main/java/com/garciaericn/t2d/fragments/SignUpFragment.java
@@ -0,0 +1,15 @@
+package com.garciaericn.t2d.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Full Sail University
+ * Mobile Development BS
+ * Created by ENG618-Mac on 1/27/15.
+ */
+public class SignUpFragment extends Fragment {
+
+    public SignUpFragment() {
+        // Mandatory empty constructor
+    }
+}
